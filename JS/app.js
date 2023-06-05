38a255e61c30ebc52300d287b709955ae37cf1bf
saifyahya <saifbarakat991@gmail.com>
2023-06-05 14:11:49 +0000

    adding more questions

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,7 @@
-let userName = prompt("Enter your name: ")
+
+let welConf =confirm(" Skip welcome message?")
+if(welConf==false) {
+    let userName = prompt("Enter your name: ")
 
 let userGender = prompt("Enter your gender: male/female")
 
@@ -9,9 +12,6 @@ while (userAge<=0 && userAge!=null) {
     userAge = prompt("Enter your age: ")
 }
 
-let welConf =confirm(" Skip welcome message?")
-
-if(welConf==false) {
     if(userGender=="male"||userGender=="Male"){
         alert("Welcome Mr."+userName)
     }
@@ -22,3 +22,32 @@ if(welConf==false) {
         alert("Welcome "+userName)
     }
 }
+
+
+
+ let myArray=[]
+
+let q1= prompt("Do you have a job: Yes/No")
+let q2= prompt("Do you have many daily tasks: Yes/No")
+let q3= prompt("Do you like to order your tasks: Yes/No")
+
+const arrAnswers =()=> {
+myArray.push(q1)
+myArray.push(q2)
+myArray.push(q3)    
+} 
+
+const arrModify =()=> {
+    for(let i = 0 ; i < myArray.length ; i++)
+    if (myArray[i]==null)
+    myArray[i]="invalid"
+}
+
+const arrTrev =()=> {
+    for(let i =0;i<myArray.length;i++)
+    console.log(myArray[i])
+}
+
+arrAnswers()
+arrModify()
+arrTrev()
